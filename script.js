98e988c71b046f28f83c6cbbc9aaf18fda6f7e15
Almog Mizrahi <almogosh111@gmail.com>
2026-04-05 13:56:34 +0300

    change service card in  html and css

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+// main j
+
 function toggleService(card) {
     const isActive = card.classList.contains('active');
 
-  // סגור את כל הכרטיסיות
-document.querySelectorAll('.service-card')
-.forEach(c => c.classList.remove('active'));
+    document.querySelectorAll('.service-card')
+            .forEach(c => c.classList.remove('active'));
 
-// פתח את זו שנלחצה (אם לא הייתה פתוחה)
-if (!isActive) {
-    card.classList.add('active');
-}
+    if (!isActive) {
+        card.classList.add('active');
+    }
 }
